09de05e1f5b0503b5abffd87f0e4bdb2734c1484
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-31 10:12:49 -0300

    change root route and add cookies[:page]

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,5 +1,10 @@
 class PersonsController < ApplicationController
   def index
     @persons = Person.all.page(params[:page])
+    cookies[:page] = params[:page]
+  end
+
+  def show
+    @person = Person.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "persons#index"
+  root to: "persons#index"
   resources :persons
 end
