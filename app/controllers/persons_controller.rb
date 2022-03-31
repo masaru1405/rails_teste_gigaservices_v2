0defe89a968338b215fff144966cb7ba0389aad1
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-03-31 13:48:06 -0300

    add busca

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -9,6 +9,18 @@ class PersonsController < ApplicationController
   end
 
   def busca
-
+    cookies[:page] = params[:page]
+    if params[:first_name] or params[:last_name] or params[:email] or params[:gender]
+      result = Person.search(params[:first_name], params[:last_name], params[:email], params[:gender])
+      @total = result.count
+      @persons_result = result.page(params[:page])
+    else
+      @persons_result = nil
+    end
   end
+
+  private
+    def persons_params
+      params.require(:person).permit(:first_name, :last_name, :email, :gender)
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,29 @@
 class Person < ApplicationRecord
   paginates_per 10
+
+  def self.search(first_name, last_name, email, gender)
+    if gender == nil or gender == ""
+      person = Person.where(
+        "lower(first_name) LIKE ? and 
+        lower(last_name) LIKE ? and 
+        lower(email) LIKE ?",
+        "%#{first_name.downcase}%",
+        "%#{last_name.downcase}%",
+        "%#{email.downcase}%"
+      )
+    else
+      person = Person.where(
+        "lower(first_name) LIKE ? and 
+         lower(last_name) LIKE ? and 
+         lower(email) LIKE ? and 
+         gender LIKE ?",
+         "%#{first_name.downcase}%",
+         "%#{last_name.downcase}%",
+         "%#{email.downcase}%",
+         "%#{gender}%"
+      )
+    end
+    return person
+  end
+
 end
